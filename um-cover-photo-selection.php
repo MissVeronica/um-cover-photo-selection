0a5ede0b71ac2aa6606d6458448670ea2bd14304
MissVeronica <veronica@tranza.se>
2023-04-10 09:10:41 +0200

    Update um-cover-photo-selection.php

--- um-cover-photo-selection.php
+++ um-cover-photo-selection.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - Cover Photo Selection
  * Description:     Extension to Ultimate Member for User Cover Photo Selection from Site Predefined Photos.
- * Version:         2.1.0
+ * Version:         2.2.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
@@ -18,41 +18,66 @@ if ( ! class_exists( 'UM' ) ) return;
 
 class UM_Cover_Photo_Selection {
 
-    public $output  = '';
-    public $url     = '';
-    public $subdir  = '';
-    public $title   = '';
-    public $form_id = '';
-    public $message = '';
-    public $data_ratio   = '';
-    public $cover_photos = 'cover-photos';
-    public $meta_key     = 'my_cover_photo';
+    public $output     = '';
+    public $url        = '';
+    public $subdir     = '';
+    public $title      = '';
+    public $message    = '';
+    public $data_ratio = '';
+
+    public $form_id          = false;
+    public $cover_photos     = 'cover-photos';
+    public $meta_key         = 'my_cover_photo';
     public $photo_extensions = array();
     public $selection_list   = array();
+    public $fields           = array();
+    public $um_profile_role  = array();
+    public $custom_settings  = false;
 
     function __construct( ) {
 
-        add_shortcode( 'cover_photo_selection' ,            array( $this, 'cover_photo_selection_shortcode' ));
+        $this->form_id = sanitize_text_field( UM()->options()->get( 'cover_photo_selection_form_id' ));
 
-        add_action( 'um_profile_before_header',             array( $this, 'um_init_my_predefined_cover_photo' ), 10 );
-        add_filter( 'um_get_default_cover_uri_filter',      array( $this, 'um_get_default_cover_uri_filter_selection' ), 10, 1 );
-        add_filter( 'um_user_cover_photo_uri__filter',      array( $this, 'um_get_default_cover_uri_filter_selection' ), 10, 1 );
-        add_filter( 'um_myprofile_edit_menu_items',         array( $this, 'um_myprofile_edit_menu_items_photo_selection' ), 10, 1 );
-        add_filter( 'um_profile_edit_menu_items',           array( $this, 'um_myprofile_edit_menu_items_photo_selection' ), 10, 1 );
-        add_action( 'um_before_upload_db_meta_cover_photo', array( $this, 'um_before_upload_db_meta_cover_photo_selection' ), 10, 1 );
+        if ( ! empty( $this->form_id ) && is_numeric( $this->form_id )) {
 
-        add_filter( 'um_settings_structure',                array( $this, 'um_settings_structure_cover_photo_selection' ), 10, 1 );
-        add_filter( 'um_predefined_fields_hook',            array( $this, 'um_predefined_fields_cover_photo_selection' ), 10, 1 );
-        add_filter( 'um_admin_pre_save_field_to_form',      array( $this, 'um_admin_pre_save_field_to_form_photo_selection' ), 10, 1 );
+            $this->fields = UM()->query()->get_attr( 'custom_fields', $this->form_id );
+            $form_type = get_post_meta( $this->form_id, '_um_mode', true );
 
-        add_filter( 'um_profile_tabs',                      array( $this, 'profile_tabs' ), 10, 1 );
+            if ( ! array_key_exists( $this->meta_key, $this->fields ) || $form_type != 'profile' ) {
+                $this->form_id = false;
+                $this->fields  = false;
+            }
+ 
+        } else $this->form_id = false;
 
-        add_filter( "um_{$this->meta_key}_predefined_field_options", array( $this, 'my_cover_photo_predefined_field_options' ), 10, 1 );
-        add_filter( "um_get_field__{$this->meta_key}",               array( $this, 'my_cover_photo_get_field_options' ), 10, 1 );
+        add_shortcode( 'cover_photo_selection' , array( $this, 'cover_photo_selection_shortcode' ));
+
+        if ( $this->form_id ) {
+
+            $this->um_profile_role = get_post_meta( $this->form_id, '_um_profile_role', true );
+            $this->custom_settings = get_post_meta( $this->form_id, '_um_profile_use_custom_settings', true );
+
+            add_action( 'um_profile_before_header',             array( $this, 'um_init_my_predefined_cover_photo' ), 10 );
+            add_filter( 'um_get_default_cover_uri_filter',      array( $this, 'um_get_default_cover_uri_filter_selection' ), 10, 1 );
+            add_filter( 'um_user_cover_photo_uri__filter',      array( $this, 'um_get_default_cover_uri_filter_selection' ), 10, 1 );
+            add_filter( 'um_myprofile_edit_menu_items',         array( $this, 'um_myprofile_edit_menu_items_photo_selection' ), 10, 1 );
+            add_filter( 'um_profile_edit_menu_items',           array( $this, 'um_myprofile_edit_menu_items_photo_selection' ), 10, 1 );
+            add_action( 'um_before_upload_db_meta_cover_photo', array( $this, 'um_before_upload_db_meta_cover_photo_selection' ), 10, 1 );
+        } 
+
+        if( is_admin() ) {
+
+            add_filter( 'um_settings_structure',           array( $this, 'um_settings_structure_cover_photo_selection' ), 10, 1 );
+            add_filter( 'um_predefined_fields_hook',       array( $this, 'um_predefined_fields_cover_photo_selection' ), 10, 1 );
+            add_filter( 'um_admin_pre_save_field_to_form', array( $this, 'um_admin_pre_save_field_to_form_photo_selection' ), 10, 1 );
+        }
+
+        add_filter( "um_get_field__{$this->meta_key}",     array( $this, 'my_cover_photo_get_field_options' ), 10, 1 );
+        add_filter( 'um_profile_tabs',                     array( $this, 'profile_tabs' ), 10, 1 );
 
         $extensions = UM()->options()->get( 'cover_photo_selection_extensions' );
         if ( empty( $extensions )) {
-            $extensions = 'jpg,jpeg,png';
+            $extensions = 'jpg,jpeg,png,webp';
         }
 
         $this->photo_extensions = array_map( 'trim', array_map( 'sanitize_text_field', explode( ",", $extensions )));
@@ -60,7 +85,6 @@ class UM_Cover_Photo_Selection {
         $this->url              = UM()->uploader()->get_upload_base_url() . $this->cover_photos . DIRECTORY_SEPARATOR;
         $this->data_ratio       = 'data-ratio="' . esc_attr( UM()->options()->get( 'profile_cover_ratio' ) ) . '"';
         $this->title            = __( 'Cover Photo name and the number of users using this Cover Photo', 'ultimate-member' );
-        $this->form_id          = sanitize_text_field( UM()->options()->get( 'cover_photo_selection_form_id' ));
     }
 
     public function get_selection_list() {
@@ -89,21 +113,19 @@ class UM_Cover_Photo_Selection {
                 asort( $this->selection_list );
             }
 
-            $fields = UM()->query()->get_attr( 'custom_fields', $this->form_id );
-
             if ( ! empty( $this->selection_list ) && is_array( $this->selection_list )) {
 
-                if ( is_array( $fields[$this->meta_key]['options'] )) {
+                if ( isset( $this->fields[$this->meta_key]['options'] ) && is_array( $this->fields[$this->meta_key]['options'] )) {
 
-                    $diff_1 = array_diff( $this->selection_list,               $fields[$this->meta_key]['options'] ); 
-                    $diff_2 = array_diff( $fields[$this->meta_key]['options'], $this->selection_list );
+                    $diff_1 = array_diff( $this->selection_list,                     $this->fields[$this->meta_key]['options'] ); 
+                    $diff_2 = array_diff( $this->fields[$this->meta_key]['options'], $this->selection_list );
 
                     if ( count( $diff_1 ) > 0 || count( $diff_2 ) > 0 ) {
 
-                        $fields[$this->meta_key]['options'] = array_merge( $diff_1, array_diff( $fields[$this->meta_key]['options'], $diff_2 ));
-                        sort( $fields[$this->meta_key]['options'] );
+                        $this->fields[$this->meta_key]['options'] = array_merge( $diff_1, array_diff( $this->fields[$this->meta_key]['options'], $diff_2 ));
+                        sort( $this->fields[$this->meta_key]['options'] );
 
-                        UM()->query()->update_attr( 'custom_fields', $this->form_id, $fields );
+                        UM()->query()->update_attr( 'custom_fields', $this->form_id, $this->fields );
                     }
                 }
 
@@ -116,8 +138,8 @@ class UM_Cover_Photo_Selection {
                 $dropdown_message[] = __( 'Target uploads folder: ', 'ultimate-member' );
                 $dropdown_message[] = esc_attr( str_replace( ABSPATH, '...', $this->subdir ));
 
-                $fields[$this->meta_key]['options'] = $dropdown_message;
-                UM()->query()->update_attr( 'custom_fields', $this->form_id, $fields );
+                $this->fields[$this->meta_key]['options'] = $dropdown_message;
+                UM()->query()->update_attr( 'custom_fields', $this->form_id, $this->fields );
 
                 $this->message = $dropdown_message[0];
             }
@@ -127,10 +149,8 @@ class UM_Cover_Photo_Selection {
             $new_dir = $wp_filesystem->mkdir( $this->subdir );
 
             if ( ! $new_dir ) {
-                return new \WP_Error( 'FilesysError', __( 'Failed to create the new Cover Photos folder.', 'ultimate-member' ) );
+                return new \WP_Error( 'FilesysError', __( 'Failed to create the new Predefined Cover Photos folder.', 'ultimate-member' ) );
             }
-
-            $this->message = __( 'New Predefined Cover Photos folder created OK', 'ultimate-member' );
         }
     }
 
@@ -150,7 +170,7 @@ class UM_Cover_Photo_Selection {
                         $users_count = (int) $user_query->get_total();
 
                         $html[$file] = '
-                                
+
                                 <div class="um-field-label">
                                     <label title="' . $this->title . '">' . $file . ' - ' . esc_attr( $users_count ) . ' </label>
                                     <div class="um-clear"></div>
@@ -174,6 +194,7 @@ class UM_Cover_Photo_Selection {
     public function um_get_default_cover_uri_filter_selection( $uri ) {
 
         $my_cover_photo = um_user( $this->meta_key );
+        $this->get_selection_list();
 
         if ( ! empty( $my_cover_photo ) 
                && is_array( $this->selection_list )
@@ -191,15 +212,15 @@ class UM_Cover_Photo_Selection {
 
     public function cover_photo_selection_shortcode( $atts, $option ) {
 
+        if ( ! empty( $this->message )) {
+
+            return __( 'Shortcode Message: ', 'ultimate-member' ) . $this->message;
+        }
+
         if ( ! empty( $this->output )) {
             $this->output = str_replace( '{data_user_id}', 'data-user_id="' . esc_attr( um_profile_id()) . '"', $this->output );
 
             return $this->output;
-        }
-
-        if ( ! empty( $this->message )) {
-
-            return __( 'Shortcode Message: ', 'ultimate-member' ) . $this->message;
         }
 
         return '';
@@ -247,21 +268,20 @@ class UM_Cover_Photo_Selection {
         return $predefined_fields;
     }
 
-    public function my_cover_photo_predefined_field_options( $array ) {
-
-        $this->get_selection_list();
-
-        return $this->selection_list;
-    }
-
     public function um_myprofile_edit_menu_items_photo_selection( $items ) {
 
         global $current_user;
 
         $user_can_change_cover = false;
 
-        if ( $current_user->ID == um_profile_id() ) {
+        if ( $current_user->ID == um_profile_id()) {
             $user_can_change_cover = true;
+
+            if ( $this->custom_settings == 1 ) {
+                if ( ! in_array( UM()->user()->get_role() , $this->um_profile_role )) {
+                    $user_can_change_cover = false;
+                }
+            } 
 
         } else {
 
@@ -271,25 +291,18 @@ class UM_Cover_Photo_Selection {
 
         if ( $user_can_change_cover ) {
 
-            if ( ! empty( $this->form_id )) {
+            $subnav_form = '&amp;subnav=profileform-' . esc_attr( $this->form_id );
+            $new_items = array();
 
-                $subnav_form = '&amp;subnav=profileform-' . esc_attr( $this->form_id );
-                $new_items = array();
+            foreach( $items as $key => $item ) {
 
-                foreach( $items as $key => $item ) {
-
-                    $new_items[$key] = $item;
-                    if ( $key == 'editprofile' ) {
-                        $new_items['photo_selection'] = '<a href="' . esc_url( um_edit_profile_url() ) . $subnav_form .'" class="real_url">' . __( 'Cover Photo', 'ultimate-member' ) . '</a>';
-                    }
+                $new_items[$key] = $item;
+                if ( $key == 'editprofile' ) {
+                    $new_items['photo_selection'] = '<a href="' . esc_url( um_edit_profile_url() ) . $subnav_form . '" class="real_url">' . __( 'Cover Photo', 'ultimate-member' ) . '</a>';
                 }
-
-                return $new_items;
-
-            } else {
-
-                $items['error'] = '<div style="font-size:14px;">* ' . __( 'No Predefined Cover Photos Form ID found', 'ultimate-member' ) . ' *</div>';
             }
+
+            return $new_items;
         }
 
         return $items;
@@ -297,7 +310,7 @@ class UM_Cover_Photo_Selection {
 
     public function um_before_upload_db_meta_cover_photo_selection( $user_id ) {
 
-        if ( ! empty( um_user( $this->meta_key ))) {
+        if ( $this->meta_key && ! empty( um_user( $this->meta_key ))) {
 
             update_user_meta( $user_id, $this->meta_key, '' );
             UM()->user()->remove_cache( $user_id );
@@ -308,10 +321,10 @@ class UM_Cover_Photo_Selection {
     public function um_admin_pre_save_field_to_form_photo_selection( $field_args ) {
 
         if ( $field_args['metakey'] == $this->meta_key ) {
-            
-            $this->get_selection_list();
-            $field_args['options'] = $this->selection_list;
-            sort( $field_args['options'] );
+
+                $this->get_selection_list();
+                $field_args['options'] = $this->selection_list;
+                sort( $field_args['options'] );
         }
 
         return $field_args;
